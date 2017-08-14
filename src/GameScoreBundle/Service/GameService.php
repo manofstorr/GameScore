7c5626f7a6627279f63eab543892f8d3fdceeaea
David <david.baudry@zoho.com>
2017-08-14 13:27:13 +0200

    deleted unusued property container in game service

--- src/GameScoreBundle/Service/GameService.php
+++ src/GameScoreBundle/Service/GameService.php
@@ -18,13 +18,11 @@ use Symfony\Component\DependencyInjection\Container;
 class GameService
 {
     protected $em;
-    private $container;
 
     // We need to inject this variables later.
-    public function __construct(EntityManager $entityManager, Container $container)
+    public function __construct(EntityManager $entityManager)
     {
         $this->em = $entityManager;
-        $this->container = $container;
     }
 
     public function getTotalNumberOfGames() {
