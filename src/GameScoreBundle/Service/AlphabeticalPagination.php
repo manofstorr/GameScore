67498cd60481526d6157c42027a0121cc93849fb
David <david.baudry@zoho.com>
2017-08-14 13:28:13 +0200

    deleted unusued property container in alphabeticalpagination service

--- src/GameScoreBundle/Service/AlphabeticalPagination.php
+++ src/GameScoreBundle/Service/AlphabeticalPagination.php
@@ -17,10 +17,9 @@ class AlphabeticalPagination
     private $container;
 
     // We need to inject this variables later.
-    public function __construct(EntityManager $entityManager, Container $container)
+    public function __construct(EntityManager $entityManager)
     {
         $this->em = $entityManager;
-        $this->container = $container;
     }
 
     public function getAlphabeticalPagination($entityname)
