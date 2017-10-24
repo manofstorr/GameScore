889140a0736f65684beb959e1fa9f40d9f648de1
David Baudry <david.baudry@zoho.com>
2017-10-24 17:18:14 +0200

    Added : More tests and dataProvider on helperservice test

--- src/GameScoreBundle/Service/HelperService.php
+++ src/GameScoreBundle/Service/HelperService.php
@@ -16,6 +16,7 @@ class HelperService
     {
         $delimiter = '#';
         $pattern = '^[\d]{4}\-[\d]{2}-[\d]{2}\s[\d]{2}:[\d]{2}:[\d]{2}$';
+        // todo : make validation on sub-strings : month between 01 and 12 etc
         $test = preg_match($delimiter . $pattern . $delimiter, $datetimeStringToValidate, $matches);
         return ($test === 1);
     }
--- tests/GameScoreBundle/Service/HelperServiceTest.php
+++ tests/GameScoreBundle/Service/HelperServiceTest.php
@@ -14,11 +14,45 @@ use GameScoreBundle\Service\HelperService;
 class HelperServiceTest extends TestCase
 {
 
-    public function testDatetimeStringValidator()
+    /**
+     * @dataProvider valuesForDatetimeStringValidatorOK
+     */
+    public function testDatetimeStringValidatorOK($dateString)
     {
         $helperService = new HelperService();
-        $dateString = "2012-12-12 00:00:01";
         $this->assertTrue($helperService->datetimeStringValidator($dateString));
     }
+
+    /**
+     * @dataProvider valuesForDatetimeStringValidatorNOK
+     */
+    public function testDatetimeStringValidatorNOK($dateString)
+    {
+        $helperService = new HelperService();
+        $this->assertFalse($helperService->datetimeStringValidator($dateString));
+    }
+
+    public function valuesForDatetimeStringValidatorOK()
+    {
+        return [
+            ["1982-01-04 12:12:31"],
+            ["2016-11-10 10:00:00"],
+            ["2016-12-10 10:05:00"]
+        ];
+    }
+
+    public function valuesForDatetimeStringValidatorNOK()
+    {
+        return [
+            ["Bonjour"],
+            ["100-01-04 12:12:31"],
+            ["2016-13-10 10:00:00"],
+            ["2016-12-10 10:70:00"],
+            ["2016-12-10 43:00:00"],
+            ["2016-12-10 12:00:76"]
+        ];
+    }
+
+
 
 }
